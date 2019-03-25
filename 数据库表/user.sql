9d3bea8b05f7870a4d1d96c130afbb7750cb3931
zww <zhangweiwei@ejie365.com>
2019-03-25 11:56:13 +0800

    Signed-off-by: zww <zhangweiwei@ejie365.com>

---
+++ 数据库表/user.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user`(`id`, `name`, `nature`) VALUES ('1', '陶喆', '0');
+INSERT INTO `user`(`id`, `name`, `nature`) VALUES ('2', '王力宏', '1');
+INSERT INTO `user`(`id`, `name`, `nature`) VALUES ('3', '周杰伦', '2');
+INSERT INTO `user`(`id`, `name`, `nature`) VALUES ('4', '陈绮贞', '3');
